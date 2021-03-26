b2ea03735388789b9debd125b55ecb525bdb7760
Brenno Natal <natalbrenno@gmail.com>
2021-03-26 12:42:00 -0300

    Fixing bug on import and printing updated itens

--- infra/update.py
+++ infra/update.py
@@ -4,6 +4,7 @@ from infra.apiKey import api_key
 from infra.connection import Database
 # import pandas as pd
 import json
+from jsondiff import diff
 
 api = Api(api_key)
 class Import():
@@ -11,7 +12,7 @@ class Import():
 	def products(self):
 		try:
 			# opening database
-			db = Database()
+			# db = Database()
 			# getting products from api
 			products = api.get_products()
 			# opening file with products records
@@ -21,8 +22,11 @@ class Import():
 			# checking every product
 			for product in products:
 				# checking if product was imported already
-				if records[product['id']] != product or not records[product['id']]:
-					print(f'Updated: {product["codigo"]}')
+				if product['id'] not in records or records[product['id']] != product:
+					print(f'UPDATE: {product["id"]}\n')
+					print(f'\tOLD: {diff(product, records[product["id"]])}\n')
+					print(f'\tNEW: {diff(records[product["id"]], product)}\n')
+
 					records[product['id']] = product
 					p_file = open("infra/imported/produtos.json", "w")
 					json.dump(records, p_file)
@@ -31,102 +35,35 @@ class Import():
 				
 		except ApiError as e:
 			print(e.response)
-		db.close()
+		# db.close()
 		
+
 	def orders(self):
 		try:
-			db = Database()
+			# db = Database()
 			orders = api.get_orders()
-			df = pd.DataFrame(orders)
+			# opening file with order records
+			o_file = open("infra/imported/pedidos.json", "r")
+			records = json.load(o_file)
+			o_file.close()
+			# checking every order
+			for order in orders:
+				# checking if order was imported already
+				if order['numero'] not in records or records[order['numero']] != order:
+					print(f'UPDATE: {order["numero"]}\n')
+					print(f'\tOLD: {diff(order, records[order["numero"]])}\n')
+					print(f'\tNEW: {diff(records[order["numero"]], order)}\n')
 
-
-			desc = df.pop("desconto")
-			desc.to_json('desconto.json')
-
-			data = df.pop("data")
-			data.to_json('data.json')
-
-			nume = df.pop("numero")
-			nume.to_json('numero.json')
-
-			nocp = df.pop("numeroOrdemCompra")
-			nocp.to_json('nocp.json')
-
-			vndr = df.pop('vendedor')
-			vndr.to_json('vendedor.json')
-
-			vlfr = df.pop("valorfrete")
-			vlfr.to_json('valfrete.json')
-
-			situ = df.pop('situacao')
-			situ.to_json('situ.json')
-
-			dtsd = df.pop('dataSaida')
-			dtsd.to_json('DTsaida.json')
-
-			loja = df.pop('loja')
-			loja.to_json('loja.json')
-
-			clnt = df.pop("cliente")
-			clnt.to_json('cliente.json')
-
-			pgmt = df.pop('pagamento')
-			pgmt.to_json('pagamento.json')
-
-			itns = df.pop("itens")
-			itns.to_json('itens.json')
-
-			pcls = df.pop('parcelas')
-			pcls.to_json('parcelas.json')
-
-
-
-			# dfa = df.pd.DataFrame.colums("desconto","data","numero","numeroOrdemCompra","vendedor","valorfrete","situacao","dataSaida","loja","cliente","pagamento","itens","parcelas","dataPrevista")
-			print(desc)
-			# cli.to_sql('cliente', db.encnx, index = False , schema = 'destripida', if_exists = 'append')
-
-			# df.to_json('orders.json')
-
-			# df_par = df.drop(labels= 'parcelas',axis = 1, inplace=True)
-			# df.drop(labels= 'observacoes',axis = 1, inplace=True)
-			# df.drop(labels= 'observacaointerna',axis = 1, inplace=True)
-			# df.drop(labels= 'dataPrevista',axis = 1, inplace=True)
-			# df_pag = df.drop(labels= 'pagamento',axis = 1, inplace=True)
-			# df.drop(labels= 'numeroOrdemCompra',axis = 1, inplace=True)
-
-			
-
-
-			# print(df.itens)
-			
-
-			# print(df_par)
-
-			# for o in orders:
-			# 	f = o.pop('cliente')
-			# 	i = o.pop ('itens')
-			# 	m = o.pop 
-
-			# 	j = o.pop('forma_pagamento')
-			# 	print(o)
-			# 	df = pd.DataFrame(o)
-			# 	o.to_csv('pedido.csv', index=True)
-			# 	dff =pd.DataFrame(j)
-			# 	dff.to_csv('forma_pagamento.csv', index=False)
-			# 	df0 = pd.DataFrame(f)
-			# 	print(df0)
-			# 	df0.to_csv('clientes.csv', index=True)
-			# 	df1 = pd.DataFrame(i)
-			# 	df1.to_csv('ítens.csv', index=False)S
-				
-			# 	print(df)
-			# 	print(o)
-			
-			# 	df.to_sql('itens', db.encnx, index = False , schema = 'destripida', if_exists = 'append')
-			# 	df1.to_sql('parcelas', db.encnx, index = False , schema = 'destripida', if_exists = 'append')
+					records[order['numero']] = order
+					o_file = open("infra/imported/pedidos.json", "w")
+					json.dump(records, o_file)
+					o_file.close()
+					# self._insert_database(table='pedido', obj=order)
 			
 		except ApiError as e:
 			print(e.response)
+		# db.close(
+
 
 	def contacts(self):
 		try:
--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from infra.update import Import
 
 update = Import()
 update.products()
+update.orders()
